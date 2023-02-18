c58e8792cdc2ee4b6056006d30e022030d5afe3c
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2023-02-18 21:04:14 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,76 +1,76 @@
 const DATA = [
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/rolled-diuner-15-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Rolled Дюнер",
         "weight": 500,
         "category": "Дюнери",
         "price": 6.99
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/diuner-2-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер среден",
         "weight": 250,
         "category": "Дюнери",
         "price": 4.4
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/diuner-2-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер голям",
         "weight": 350,
         "category": "Дюнери",
         "price": 6.2
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/diuner-2-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер XL",
         "weight": 500,
         "category": "Дюнери",
         "price": 7.4
     },
     {
-        "image": "parcheto.bg/wp-content/uploads/2022/04/diuner-box-11-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер box",
         "weight": 400,
         "category": "Дюнери",
         "price": 6.99
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/diuner-djob-19-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер джоб",
         "weight": 600,
         "category": "Дюнери",
         "price": 6.99
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/porcia-diuner-31-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Дюнер порция",
         "weight": 700,
         "category": "Дюнери",
         "price": 8.99
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/burger-s-diuner-meso-i-kartofi-48-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Бургер с месо от дюнер и картофи",
         "weight": 350,
         "category": "Бургери",
         "price": 9.49
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/burger-s-diuner-meso-i-kartofi-48-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Бургер с телешко месо",
         "weight": 350,
         "category": "Бургери",
         "price": 9.99
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/burger-s-diuner-meso-i-kartofi-48-of-50.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Бургер с хрупкаво пилешко",
         "weight": 350,
         "category": "Бургери",
         "price": 9.49
     },
     {
-        "image": "https://www.parcheto.bg/wp-content/uploads/2022/04/hrupkavo-pile-s-garnitura-sos-01.jpg",
+        "image": "https://www.aladinfoods.bg/files/images/291/pileshki_duner_grand.png",
         "name": "Хрупкаво пиле с гарнитура и сос",
         "weight": 350,
         "category": "Предястия",
